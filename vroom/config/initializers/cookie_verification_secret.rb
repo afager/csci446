7cbd05ab88f8a10769f09573d0bafca62e563249
Andrew Fager <afager@mines.edu>
2011-02-02 09:37:48 -0700

    Adding finished project. Could not get sidebar to render properly, the code for it appears instead of the buttons/text

---
+++ vroom/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '46ed20fbfe6dd5bcbd1980c2b94d38d034c1477d7f9dc0ba213c6d2c3f05b97a30c1e32425c811ba365aa23fb575e29d59748e48bc21c9fd10b01ef4a285b30a';
